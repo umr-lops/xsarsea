cac0d63e794b62a096dbf0d8090fb67fbdba348d
Olivier Archer <olivier.archer@ifremer.fr>
2022-01-31 13:23:13 +0100

    init winddir.py

---
+++ src/xsarsea/winddir.py
@@ -0,0 +1,17 @@
+"""
+Winddir retrieval module
+"""
+
+__all__ = ['WindDirGradient']
+
+
+class WindDirGradient():
+    def __init__(self, sar_ds):
+        """
+
+        Parameters
+        ----------
+        sar_ds: `xarray.Dataset`
+            with at least `sigma0_detrend` and `ground_heading` variables
+        """
+        pass
