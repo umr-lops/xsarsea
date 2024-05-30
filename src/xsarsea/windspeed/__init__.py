1c30d2bbc8c495f46ca785080fa341d75bf87f1f
Vincent LHEUREUX <vincent.lheureux@hotmail.fr>
2024-05-30 10:15:50 +0200

    register one sarwing lut

--- src/xsarsea/windspeed/__init__.py
+++ src/xsarsea/windspeed/__init__.py
@@ -2,10 +2,10 @@
 windspeed module, for retrieving wind speed from sigma0 and models.
 """
 __all__ = ['invert_from_model', 'available_models', 'get_model', 'register_cmod7',
-           'register_all_sarwing_luts', 'register_all_nc_luts', 'nesz_flattening']
+           'register_one_sarwing_lut', 'register_all_sarwing_luts', 'register_all_nc_luts', 'nesz_flattening']
 from .windspeed import invert_from_model
 from .models import available_models, get_model, register_all_nc_luts
-from .sarwing_luts import register_all_sarwing_luts
+from .sarwing_luts import register_all_sarwing_luts, register_one_sarwing_lut
 from .cmod7 import register_cmod7
 from .utils import nesz_flattening, get_dsig
 from . import gmfs
