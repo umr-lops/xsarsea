3b0185ecfdfb58af9758ddba3a2a3359e3bbb038
LHEUREUX <vincent.lheureux@ifremer.fr>
2025-05-27 06:45:51 +0200

    test of new gmfs rs2_v4 and rcm_v4 that are respectively copied and minor changed from _v3

--- src/xsarsea/windspeed/gmfs_impl.py
+++ src/xsarsea/windspeed/gmfs_impl.py
@@ -538,19 +538,100 @@ def gmf_rcm_v3(incidence, speed, phi=None):
         linear sigma0
 
     """
-    
-    ## RCM TEST_1_20_smap_corrected
-    u10 = speed 
+
+    # RCM TEST_1_20_smap_corrected
+    u10 = speed
     inc = incidence
     a0_Z1, b0_Z1, b1_Z1 = 7.093964676135241e-06, 2.3722948391886542, -0.009516840375089524
-    a_Z1 = a0_Z1 
+    a_Z1 = a0_Z1
     b_Z1 = b0_Z1 + b1_Z1 * inc
     sig_Z1 = a_Z1 * u10 ** (b_Z1)
-    a0_Z2, a1_Z2, a2_Z2, b0_Z2, b1_Z2, b2_Z2 = 6.689451099284358e-05, -1.3956325894252652e-06, 9.227949977841212e-09, 1.4687699534267797, 0.005735224541037088, -7.164130353316848e-05 
+    a0_Z2, a1_Z2, a2_Z2, b0_Z2, b1_Z2, b2_Z2 = 6.689451099284358e-05, - \
+        1.3956325894252652e-06, 9.227949977841212e-09, 1.4687699534267797, 0.005735224541037088, - \
+        7.164130353316848e-05
     a_Z2 = a0_Z2 + a1_Z2 * inc + a2_Z2 * inc**2
     b_Z2 = b0_Z2 + b1_Z2 * inc + b2_Z2 * inc**2
     sig_Z2 = a_Z2 * u10 ** (b_Z2)
-    c0, c1, c2, c3 = -0.2454472887447197, 15.537961353644508, 0.24011368010838255, 15.332883245452303
+    c0, c1, c2, c3 = - \
+        0.2454472887447197, 15.537961353644508, 0.24011368010838255, 15.332883245452303
+    sigmoid1 = 1 / (1 + np.exp(-c0 * (u10 - c1)))
+    sigmoid2 = 1 / (1 + np.exp(-c2 * (u10 - c3)))
+    return 10**((10 * np.log10(sig_Z1) * sigmoid1 + 10 * np.log10(sig_Z2) * sigmoid2) / 10)
+
+
+@GmfModel.register(wspd_range=[3.0, 80.0], pol="VH", units="linear", defer=False)
+def gmf_rcm_v4(incidence, speed, phi=None):
+    """
+    Radarsat Consteallation Mission VH GMF : relation between sigma0, incidence and windspeed.
+    Gmf gmf_rcm_v3 modified with a minor correection to b0_Z2.
+    Parameters
+    ----------
+    incidence: xarray.DataArray
+        incidence angle [deg]
+    speed: xarray.DataArray
+        wind speed [m/s]
+
+    Returns
+    -------
+    sigma0: xarray.DataArray
+        linear sigma0
+
+    """
+
+    # RCM TEST_1_20_smap_corrected
+    u10 = speed
+    inc = incidence
+    a0_Z1, b0_Z1, b1_Z1 = 7.093964676135241e-06, 2.3722948391886542, -0.009516840375089524
+    a_Z1 = a0_Z1
+    b_Z1 = b0_Z1 + b1_Z1 * inc
+    sig_Z1 = a_Z1 * u10 ** (b_Z1)
+    a0_Z2, a1_Z2, a2_Z2, b0_Z2, b1_Z2, b2_Z2 = 6.689451099284358e-05, - \
+        1.3956325894252652e-06, 9.227949977841212e-09, 1.4687699534267797, 0.005735224541037088, - \
+        7.164130353316848e-05
+    a_Z2 = a0_Z2 + a1_Z2 * inc + a2_Z2 * inc**2
+    b_Z2 = b0_Z2*1.01 + b1_Z2 * inc + b2_Z2 * inc**2
+    sig_Z2 = a_Z2 * u10 ** (b_Z2)
+    c0, c1, c2, c3 = - \
+        0.2454472887447197, 15.537961353644508, 0.24011368010838255, 15.332883245452303
+    sigmoid1 = 1 / (1 + np.exp(-c0 * (u10 - c1)))
+    sigmoid2 = 1 / (1 + np.exp(-c2 * (u10 - c3)))
+    return 10**((10 * np.log10(sig_Z1) * sigmoid1 + 10 * np.log10(sig_Z2) * sigmoid2) / 10)
+
+
+@GmfModel.register(wspd_range=[3.0, 80.0], pol="VH", units="linear", defer=False)
+def gmf_rs2_v4(incidence, speed, phi=None):
+    """
+    Radarsat-2 VH GMF : relation between sigma0, incidence and windspeed.
+    Gmf gmf_rs2_v3 modified with a minor correection to b0_Z2.
+
+    Parameters
+    ----------
+    incidence: xarray.DataArray
+        incidence angle [deg]
+    speed: xarray.DataArray
+        wind speed [m/s]
+
+    Returns
+    -------
+    sigma0: xarray.DataArray
+        linear sigma0
+
+    """
+    # RS2 TEST_1_17.5_smap_corrected
+    u10 = speed
+    inc = incidence
+    a0_Z1, b0_Z1, b1_Z1 = 8.423384272498706e-06, 2.4351127340627374, -0.01450322326682606
+    a_Z1 = a0_Z1
+    b_Z1 = b0_Z1 + b1_Z1 * inc
+    sig_Z1 = a_Z1 * u10 ** (b_Z1)
+    a0_Z2, a1_Z2, a2_Z2, b0_Z2, b1_Z2, b2_Z2 = 0.00014955206131320428, - \
+        4.737691852310481e-06, 3.813107432709729e-08, 1.524883207000445, - \
+        0.01322253424944054, 0.00037527120092119504
+    a_Z2 = a0_Z2 + a1_Z2 * inc + a2_Z2 * inc**2
+    b_Z2 = b0_Z2*1.01 + b1_Z2 * inc + b2_Z2 * inc**2
+    sig_Z2 = a_Z2 * u10 ** (b_Z2)
+    c0, c1, c2, c3 = - \
+        0.2222881984904166, 13.118282628673661, 0.21426139278646567, 12.768845054319682
     sigmoid1 = 1 / (1 + np.exp(-c0 * (u10 - c1)))
     sigmoid2 = 1 / (1 + np.exp(-c2 * (u10 - c3)))
     return 10**((10 * np.log10(sig_Z1) * sigmoid1 + 10 * np.log10(sig_Z2) * sigmoid2) / 10)
