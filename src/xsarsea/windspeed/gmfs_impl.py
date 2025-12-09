0d71cb3ce4d3a33cbcf1c9f988ea9a646298dcfa
Chloe HABASQUE <chabasqu@compute-101-23.ifremer.fr>
2025-12-09 13:01:33 +0000

    use of function cmod5n

--- src/xsarsea/windspeed/gmfs_impl.py
+++ src/xsarsea/windspeed/gmfs_impl.py
@@ -82,7 +82,6 @@ def gmf_cmod5_generic(neutral=False):
         )
         name = "gmf_cmod5n"
 
-    @GmfModel.register(name, wspd_range=[0.2, 50.0], pol="VV", units="linear", defer=False)
     def gmf_cmod5(inc, wspd, phi):
         zpow = 1.6
         thetm = 40.0
@@ -132,12 +131,20 @@ def gmf_cmod5_generic(neutral=False):
         sig = b0 * (1.0 + b1 * cosphi + b2 * (2.0 * cosphi**2.0 - 1.0)) ** zpow
         return sig
 
-    return gmf_cmod5
+    return gmf_cmod5, name
 
 
 # register gmfs gmf_cmod5 and gmf_cmod5n
-gmf_cmod5_generic(neutral=False)
-gmf_cmod5_generic(neutral=True)
+for neutral in [True, False]:
+    gmf, name = gmf_cmod5_generic(neutral=neutral)
+    GmfModel.register(
+        name,
+         wspd_range=[0.2, 50.0],
+         pol="VV",
+         units="linear",
+         defer=False
+    )(gmf)
+
 
 
 @GmfModel.register(wspd_range=[0.2, 50.0], pol="VV", units="linear", defer=False)
@@ -657,88 +664,7 @@ def gmf_cmod5_pr_zhangA(inc, wspd, phi=None):
 
     """
 
-    c = np.array(
-        [
-            0.0,
-            -0.6878,
-            -0.7957,
-            0.338,
-            -0.1728,
-            0.0,
-            0.004,
-            0.1103,
-            0.0159,
-            6.7329,
-            2.7713,
-            -2.2885,
-            0.4971,
-            -0.725,
-            0.045,
-            0.0066,
-            0.3222,
-            0.012,
-            22.7,
-            2.0813,
-            3.0,
-            8.3659,
-            -3.3428,
-            1.3236,
-            6.2437,
-            2.3893,
-            0.3249,
-            4.159,
-            1.693,
-        ]
-    )
-
-    def gmf_cmod5(inc, wspd, phi):
-        zpow = 1.6
-        thetm = 40.0
-        thethr = 25.0
-        y0 = c[19]
-        pn = c[20]
-        a = y0 - (y0 - 1.0) / pn
-        b = 1.0 / (pn * (y0 - 1.0) ** (pn - 1.0))
-
-        # Angles
-        cosphi = np.cos(np.deg2rad(phi))
-        x = (inc - thetm) / thethr
-        x2 = x**2.0
-
-        # B0 term
-        a0 = c[1] + c[2] * x + c[3] * x2 + c[4] * x * x2
-        a1 = c[5] + c[6] * x
-        a2 = c[7] + c[8] * x
-        gam = c[9] + c[10] * x + c[11] * x2
-        s0 = c[12] + c[13] * x
-        s = a2 * wspd
-        a3 = 1.0 / (1.0 + np.exp(-s0))
-
-        if s < s0:
-            a3 = a3 * (s / s0) ** (s0 * (1.0 - a3))
-        else:
-            a3 = 1.0 / (1.0 + np.exp(-s))
-
-        b0 = (a3**gam) * 10.0 ** (a0 + a1 * wspd)
-
-        # B1 term
-        b1 = c[15] * wspd * (0.5 + x - np.tanh(4.0 * (x + c[16] + c[17] * wspd)))
-        b1 = (c[14] * (1.0 + x) - b1) / (np.exp(0.34 * (wspd - c[18])) + 1.0)
-
-        # B2 term
-        v0 = c[21] + c[22] * x + c[23] * x2
-        d1 = c[24] + c[25] * x + c[26] * x2
-        d2 = c[27] + c[28] * x
-        v2 = wspd / v0 + 1.0
-        if v2 < y0:
-            v2 = a + b * (v2 - 1.0) ** pn
-
-        b2 = (-d1 + d2 * v2) * np.exp(-v2)
-
-        # Sigma0 according to Fourier terms
-        sig = b0 * (1.0 + b1 * cosphi + b2 * (2.0 * cosphi**2.0 - 1.0)) ** zpow
-        return sig
-
+    gmf_cmod5, _ = gmf_cmod5_generic(neutral=True)
     sigma_cmod5 = gmf_cmod5(inc, wspd, phi)
 
     # PR ZhangA
